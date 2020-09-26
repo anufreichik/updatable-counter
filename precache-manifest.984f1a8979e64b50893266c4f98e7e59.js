db042d40b474117ce322fcd92aacda78fd6dd241
anufreichik <marta149@hotmail.com>
2020-09-25 20:03:36 -0700

    Updates

---
+++ precache-manifest.984f1a8979e64b50893266c4f98e7e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62e503b288833beb28f31e5ad93f8d1",
+    "url": "/updatable-counter/index.html"
+  },
+  {
+    "revision": "b343f56a701f6cd1f1f2",
+    "url": "/updatable-counter/static/css/2.dbf54c06.chunk.css"
+  },
+  {
+    "revision": "823e10698c1c8b49a245",
+    "url": "/updatable-counter/static/css/main.98912144.chunk.css"
+  },
+  {
+    "revision": "b343f56a701f6cd1f1f2",
+    "url": "/updatable-counter/static/js/2.1a9de9ba.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/updatable-counter/static/js/2.1a9de9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823e10698c1c8b49a245",
+    "url": "/updatable-counter/static/js/main.076d4838.chunk.js"
+  },
+  {
+    "revision": "2a70d13b4c8bd790c895",
+    "url": "/updatable-counter/static/js/runtime-main.1f808f03.js"
+  }
+]);
